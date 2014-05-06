90abdbca0e5222532110f9abad246d7a390bc5ba
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-05-06 14:49:11 -0500

    Initial Commit

---
+++ 100 doors in a row/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  100 doors in a row
+//
+//  Created by Mark Dumes on 5/6/14.
+//  Copyright (c) 2014 CollegeBlink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
