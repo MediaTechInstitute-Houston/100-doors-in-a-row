5adc7781d9432111c5fe56ac761c98108d1921d9
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-11-20 11:26:24 -0600

    100 Doors
    This is the solution for this warmup

--- 100 doors in a row/main.m
+++ 100 doors in a row/main.m
@@ -12,9 +12,47 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        // Changable array to add door position into
+        NSMutableArray *openClose=[[NSMutableArray alloc]init];
+        //integer to control for loop
+        int y;
+        int n;
+        int totaldivisor;
+        int numberdoors;
+        NSString *open = @"Open Door";
+        NSString *close = @"Close Door";
+        //loop to add initial state of doors
+            for (y=0; y<=99; y++) {
+                
+                [openClose addObject:close];
+                //NSLog(@"%@",openClose);
+
+                
+            }
+        //loop to open and close doors
+        for (y=1; y<=100; y++) {
+            totaldivisor = y;
+            numberdoors = (100/totaldivisor)-1;
+            //NSLog(@"%i",numberdoors);
+            int StartDoor=totaldivisor-1;
+            //nested for loop
+            for (n=0; n<=numberdoors; n++) {
+                //NSLog(@"%i",StartDoor);
+                NSString* currentdoorstate = [[NSString alloc] initWithFormat:@"%@",openClose[StartDoor]];
+                
+                if ([currentdoorstate isEqualToString:@"Open Door"] ) {
+                    [openClose replaceObjectAtIndex:StartDoor withObject:close];
+                }
+                else{
+                    [openClose replaceObjectAtIndex:StartDoor withObject:open];
+                }
+                StartDoor =(StartDoor + y);
+                            }
+        }
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        NSLog(@"%@",openClose);
         
     }
     return 0;
